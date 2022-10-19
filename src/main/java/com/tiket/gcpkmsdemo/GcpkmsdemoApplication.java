c63d5f85fd0eab49c530dbd7a356ab4bedf45eff
Abhay Joshi <abhay.joshi@tiket.com>
2022-10-19 11:25:43 +0530

    feat: added base files for project

---
+++ src/main/java/com/tiket/gcpkmsdemo/GcpkmsdemoApplication.java
@@ -0,0 +1,13 @@
+package com.tiket.gcpkmsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpkmsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpkmsdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiket/gcpkmsdemo/GcpkmsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiket.gcpkmsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcpkmsdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
